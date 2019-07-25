11971e55997bed9c78aab7d4e59c34664b69c4fa
vbc221 <vbc221@nyu.edu>
2019-07-25 10:55:12 -0500

    revisions

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
 'use strict';
 
-function getDogImage(breed) {
-  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+function getDogImage(numberPics, breed) {
+  fetch('https://dog.ceo/api/breed/' + breed + '/images/random/' + numberPics) //update fetch to handle multiple//
     .then(response => response.json())
     .then(responseJson =>
       displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+    .catch(error => console.log('Something went wrong. Try again later.'));
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
-
-  if (responseJson.message == "Breed not found") {
-    alert('That breed wasn\'t found, please try another.');
+  const numOfPics = responseJson.message.length;
+  $('.results').remove();
+  $('.user-input').after('<section class="results"></section>');
+  if (responseJson.status === 'success') {
+    for (let i = 0; i < numOfPics; i++) {
+      let currentImg = responseJson.message[i];
+      console.log(currentImg);
+      $('.results').append(`<img src=${currentImg}></img>`);
+    }
   } else {
-    //replace the existing image with the new one
-
-    $('.results').append(
-      `<img src="${responseJson.message}" class="results-img" >`);
-    //display the results section
-    $('.results').removeClass('hidden');
+    $('.results').append(`<h5>We can not find that breed</h5>`);
   }
+
+}
+
+function clearResults() {
+  $('.user-input').on('click', '#clear-results', function () {
+    $('.results').remove();
+  })
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    let breedOfDog = $('input[name="breedOfDog"]').val();
-    getDogImage(breedOfDog);
+    const numberPics = parseInt($('.numberPics').val());
+    const breed = $('.breed').val();
+    if (numberPics > 0 && numberPics < 51) {
+      getDogImage(numberPics, breed);
+    } else {
+      console.log("Incorrect number");
+    }
   });
 }
 
 $(function () {
   console.log('App loaded! Waiting for submit!');
   watchForm();
+  clearResults();
 });
